6a4f9f2acdd72d1b9dabf251b726128ecb08b111
Zhaoliang <zhz03@g.ucla.edu>
2022-07-17 22:45:22 -0700

    upload other git repos we had for blimp projects

---
+++ openblimp/git_clone.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/zhz03/Tutorial-Controlling-multiple-control-boards-through-game-controllers.git
+git clone https://gitlab.com/zhz03/blimp.git
+git clone https://gitlab.com/zhz03/dtr-spring-2022.git
+git clone https://gitlab.com/zhz03/foray.git
+git clone https://gitlab.com/zhz03/foray_spring_2022.git
+git clone https://gitlab.com/zhz03/november-2021-blimp-competition.git
+git clone https://github.com/zhz03/LEMUR_FPV_Blimp.git
+git clone https://github.com/zhz03/Blimp_PCB_design
